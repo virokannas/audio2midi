cf5a389ec555a2e66253d2f0f5f010505f4c4fe0
Simo Virokannas <svirokan@usa.wtbts.net>
2018-12-23 22:00:35 -0500

    Switched to midiutil for better handling of MIDI events. More accurate timing and note concurrency.

--- audio2midi.py
+++ audio2midi.py
@@ -4,11 +4,12 @@
 #
 
 import sys
+import os
 try:
-    import midi
+    from midiutil.MidiFile import MIDIFile
 except:
-    print("Sorry, audio2midi requires python-midi, please install it first!")
-    print("    pip install python-midi")
+    print("Sorry, audio2midi requires midiutil, please install it first!")
+    print("    pip install midiutil")
     print("")
     sys.exit(1)
 
@@ -39,16 +40,24 @@ if len(sys.argv) > 3:
 
 sampFreq, snd = wavfile.read(sys.argv[1])
 
-def file2fftmidi(track, x):
+# you can adjust BPM to match with your project to try to match the MIDI timing
+BPM = 120.0
+RESOLUTION=1
+MIDI_RES=60
+TICK_LENGTH = (60000.0 / (BPM * (MIDI_RES / RESOLUTION)))
+SLICE_LENGTH = int(sampFreq * TICK_LENGTH / 1000.0)
+print sampFreq, "resolution length",TICK_LENGTH,"ms"
+
+def file2fftmidi(mf, x):
     t=0
-    # splice audio file per 100 bytes
     t_len = len(snd)
+    print t_len,"samples",(t_len / sampFreq),"seconds"
     t_tot = 0
-    slc = 96
-    max_data = max(abs(fft(snd)))
+    slc = SLICE_LENGTH
+    nts = 0
     freqs = fftfreq(slc, 1.0/sampFreq)
     while t_tot + slc < t_len:
-        data = abs(fft(snd[t_tot:t_tot+slc]))
+        data = abs(fft(snd[t_tot:(t_tot+slc)]))
         tot = 0
 
         # find top x frequencies
@@ -56,24 +65,25 @@ def file2fftmidi(track, x):
         for tfs in top_freqs:
             freq = freqs[tfs]
             smp_data = data[tfs]
-            if smp_data > 10.0 and freq > 0.0:
-                ptc = int(12.0*math.log(float(freq)/440.0, 2.0))+69
-                vel = min(int(smp_data * 128.0 / 1000000.0),127)
-                on=midi.NoteOnEvent(tick=0, velocity=vel, pitch=ptc)
-                off=midi.NoteOffEvent(tick=1, pitch=ptc)
-                track.append(on)
-                track.append(off)
-
+            ons = []
+            if smp_data > 100.0 and freq > 0.0:
+                ptc = int(12.0 * math.log(float(freq)/440.0, 2.0)) + 69
+                if ptc<0 or ptc>127:
+                    continue
+                vel = min(int(smp_data * 128.0/1000000.0),127)
+                if vel > 0:
+                    mf.addNote(0, 0, ptc, t*RESOLUTION, RESOLUTION, vel)
+                    nts+=1
         t_tot += slc
         t+=1
+    print nts,"notes",t,"ticks",t*(TICK_LENGTH/ 1000.0),"seconds"
     return t
 
-pattern = midi.Pattern(tick_relative=False)
-track = midi.Track(tick_relative=False)
-pattern.append(track)
+mf = MIDIFile(1, eventtime_is_ticks=True, ticks_per_quarternote=MIDI_RES)
+mf.addTrackName(0, 0, "{}".format(os.path.basename(sys.argv[1])))
+mf.addTempo(0, 0, BPM)
 
-file2fftmidi(track, polyphony)
+file2fftmidi(mf, polyphony)
+with open(output_file, 'wb') as output_file:
+    mf.writeFile(output_file)
 
-eot = midi.EndOfTrackEvent(tick=1)
-track.append(eot)
-midi.write_midifile(output_file, pattern)
